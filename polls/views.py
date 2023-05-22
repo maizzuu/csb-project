62d55d476e73815409ac6592f3270a69728a1670
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2023-05-22 21:59:13 +0300

    added specific fixes

--- polls/views.py
+++ polls/views.py
@@ -23,6 +23,11 @@ def user(request):
         # you can get the information of any user by using a parameter
         # fix this by checking that parameter is the same as current user in session
         name = request.GET.get('name')
+        # fix:
+        # current_user = request.session['user']
+        # if current_user != name:
+        #   return redirect('/')
+
     try:
         user_obj = User.objects.get(username=name)
     except:
@@ -39,6 +44,7 @@ def login(request):
             if (user.password != password):
                 # A09:2021 - Security Logging and Monitoring Failures
                 # no logging with failed attempt to log in
+                # fix:
                 # print('failed to log in with username', username)
                 return redirect('/loginView')
             request.session['user'] = user.username
@@ -64,20 +70,37 @@ def admin(request):
     # A01:2021 – Broken Access Control
     # any user can access the admin endpoint that reveals all usernames and passwords
     # limit access to this to only admin
+    # fix:
+    # if request.session['user'] != 'admin':
+    #   return redirect('/')
     all_users = User.objects.all()
     return render(request, 'polls/admin.html', {'users': list(all_users)})
 
 
 def create(request):
     if request.method == 'POST':
-        # A02:2021 – Cryptographic Failures
-        # passwords are not crypted before they are saved to the database
-        # fix by using some kind of encryption
-        user = User(username=request.POST.get('username'),
-                    password=request.POST.get('password'))
+        username = request.POST.get('username')
+        password = request.POST.get('password')
         # A07:2021 – Identification and Authentication Failures
         # weak passwords allowed, same username and password allowed
         # fix by adding some type of password analysis / check
+        # fix:
+        # if username == password:
+        #    return render(request, 'polls/create.html')
+        # weak_passwords = ['password', 'password1', 'password2']
+        # if password in weak_passwords:
+        #   return render(request, 'polls/create.html')
+
+        # A02:2021 – Cryptographic Failures
+        # passwords are not crypted before they are saved to the database
+        # fix by using some kind of encryption
+        # fix with django's own encyrption:
+        # from django.contrib.auth.hashers import make_password
+        # hashed = make_password(password)
+        # user = User(username=username, password=hashed))
+
+        user = User(username=username, password=password)
+
         user.save()
         request.session['user'] = user.username
         return redirect('/')
